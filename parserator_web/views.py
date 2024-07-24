3b96d25ee39301a4a347c0cac7a2e1dbed8c9cd8
alexkcode <alexiskwan.uiuc@gmail.com>
2024-07-24 09:24:06 -0500

    attempt at javascript portion

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
+from django.views.decorators.csrf import csrf_exempt
 
 
 class Home(TemplateView):
@@ -16,17 +17,21 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        addr_param = request.GET.get('address')
+        addr_param = request.query_params.get('address')
         addr_components, addr_type = self.parse(addr_param)
-        return Response({'AddressComponents': addr_components,
-                         'AddressType': addr_type})
+        print(addr_components)
+        return Response(JSONRenderer().render({'input_string': addr_param,
+                                               'address_components': addr_components,
+                                               'address_type': addr_type}), 
+                        content_type='application/json')
+        
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
         try:
             parsed = usaddress.tag(address)
-        except TypeError:
+        except ParseError:
             raise usaddress.RepeatedLabelError
 
         address_components = parsed[0]

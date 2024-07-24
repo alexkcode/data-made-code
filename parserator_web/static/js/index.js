a023c6287fdc6caeb5774bf9753ed736b334bfda
alexkcode <alexiskwan.uiuc@gmail.com>
2024-07-24 17:12:06 -0500

    making table always refresh for every submit

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -4,44 +4,51 @@ document.addEventListener('DOMContentLoaded', (event) => {
    console.log('DOM fully loaded and parsed');
 
    document.getElementById('form').addEventListener('submit', function(event) {
-   event.preventDefault();
+      event.preventDefault();
 
-   const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+      const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-   let formData = new FormData(this);
-   let queryString = new URLSearchParams(formData).toString();
+      // remove all current table elements
+      document.querySelectorAll('.result-row').forEach(element => {
+         element.remove()
+      });
 
-   console.log('Form submitted!')
-   console.log(formData)
+      let formData = new FormData(this);
+      let queryString = new URLSearchParams(formData).toString();
 
-   fetch(`http://localhost:8000/api/parse/?${queryString}`, {
-         method: 'GET',
-         headers: {
-         'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+      console.log('Form submitted!')
+      console.log(formData)
+
+      fetch(`http://localhost:8000/api/parse/?${queryString}`, {
+            method: 'GET',
+            headers: {
+            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            }
+      })
+      .then(response => {
+            console.log('Response received:', response);
+            return response.json();
+      })
+      .then(parsedAddr => {
+         console.log('Parse Result:', parsedAddr);
+         if (parsedAddr.status === 'success') {
+            console.log('Parse success!')
+            document.getElementById('address-results').setAttribute('style', '');
+            document.getElementById('parse-type').textContent = parsedAddr.address_type
+            Object.entries(parsedAddr.address_components).map((c) => addRowToTable(c[1], c[0]))
+         } else {
+            alert('Parse Error: ' + parsedAddr.message);
          }
-   })
-   .then(response => {
-         console.log('Response received:', response);
-         return response.json();
-   })
-   .then(parsedAddr => {
-      console.log('Parse Result:', parsedAddr);
-      if (parsedAddr.status === 'success') {
-         console.log('Parse success!')
-         document.getElementById('parse-type').textContent = parsedAddr.address_type
-         Object.entries(parsedAddr.address_components).map((c) => addRowToTable(c[1], c[0]))
-      } else {
-         alert('Parse Error: ' + parsedAddr.message);
-      }
-   })
-   .catch((error) => {
-      console.error('Table Render Error:', error)
-   });
+      })
+      .catch((error) => {
+         alert('Table Render Error:', error)
+      });
    });
 
    function addRowToTable(comp, type) {
       const table = document.getElementById('address-results').getElementsByTagName('tbody')[0];
       const newRow = table.insertRow();
+      newRow.setAttribute('class', 'result-row')
 
       const addrComp = newRow.insertCell(0);
       const addrType = newRow.insertCell(1);
